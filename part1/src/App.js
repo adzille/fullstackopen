83feae03df60e4a1ef3a2c9cab10af9190b5f888
adzille <124276758+adzille@users.noreply.github.com>
2023-06-15 17:59:13 +0530

    Finished courseinfo

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,73 +1,66 @@
 
-  const Header = (props) => {
-    console.log(props)
-  
-    return(
-      <div>
-        <h1>{props.course}</h1>
-      </div>
-    )
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part= (props) => {
+  return (
+    <div>
+      <p>{props.name}: {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let total = 0;
+  props.parts.forEach(part => {total = total + part.exercises})
+  return (
+    <div>
+      <p>Number of exercises: {total}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
 
-
-  const Part = (props) => {
-    console.log(props)
-
-    return(
-      <div>
-        <p>{props.part} {props.exercises}</p>
-      </div>
-    )
-  }
-
-  
-  const Content = (props) => {
-    console.log(props)
-    
-  
-    return(
-      <div>
-        
-        <Part part = 'Fundamentals of React' exercises = '10'/>
-        <Part part = 'Using props to pass data' exercises = '7'/>
-        <Part part = 'State of a component' exercises = '14'/>
-      </div>
-    )
-  }
-  
-  const Total = (props) => {
-    console.log(props)
-    
-  
-    return(
-      <div>
-        <p>Number of exercises {props.total}</p>
-      </div>
-    )
-  
-  
-  }
-
-  
-
-  const App  = () =>{
-    const course = 'Half Stack application development'
-    
-    const total = 31
-    
-    return(
-      <div>
-        <Header course={course} />
-        <Content/>
-        <Total total ={total} />
-      </div>
-    )
-
-  }
+  return (
+    <div>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
+    </div>
+  )
+}
 
 export default App
-
-
-
-
-
